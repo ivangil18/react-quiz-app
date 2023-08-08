9c07f79c54c8f82baa9500f0adda71f69fec348f
Ivan Gil <igil.dev@gmail.com>
2023-08-08 07:59:32 -0400

    app status and startScreen implemented

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
+import Error from "./Error";
+import Loader from "./Loader";
+import StartScreen from "./StartScreen";
 
 const initialState = {
   questions: [],
@@ -22,7 +25,9 @@ function reducer(state, action) {
 }
 
 export default function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+
+  const numQuestions = questions.length;
 
   useEffect(function () {
     fetch("http://localhost:9000/questions")
@@ -35,8 +40,9 @@ export default function App() {
     <div className="app">
       <Header />
       <Main className="main">
-        <p>1/15</p>
-        <p>Question?</p>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && <StartScreen numQuestions={numQuestions} />}
       </Main>
     </div>
   );
