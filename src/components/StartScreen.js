d43e9f716daaccd660c922aec5a1d6d9aced4e86
Ivan Gil <igil.dev@gmail.com>
2023-08-17 14:09:45 -0400

    StarScreen refactored with custom hook

--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -1,9 +1,15 @@
-function StartScreen({ numQuestions, action }) {
+import { useQuiz } from "../contexts/QuizContext";
+
+function StartScreen() {
+  const { numQuestions, dispatch } = useQuiz();
   return (
     <div className="start">
       <h2>Welcome to The React Quiz!</h2>
       <h3>{numQuestions} question to test your React mastery</h3>
-      <button className="btn btn-ui" onClick={action}>
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "quiz/started" })}
+      >
         Let's Start
       </button>
     </div>
